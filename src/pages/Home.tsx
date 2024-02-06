f5c0b9cd032aece4ce9f69f522cede9784e0cd4f
martin-karani <karanimartinmk@gmail.com>
2024-02-07 02:39:42 +0300

    feat: add dynamic whitelist on nav

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,7 @@
-import PlugConnect from '@psychedelic/plug-connect';
-
-const whitelist = process.env.CANISTER_ID_FRONTEND || '';
-
-const host = 'http://127.0.0.1:4943';
-
 function Home() {
-  const handleConnectWallet = async () => {
-    console.log('Connecting to the Connect Wallet');
-    const result = await window.ic.plug.requestBalance();
-    console.log({ result });
-  };
-
   return (
     <div>
-      <h1 className="">Vite + React + Motoko</h1>
-      <div className="bg-slate-00">
-        <PlugConnect
-          title="Connect Wallet"
-          onConnectCallback={handleConnectWallet}
-          host={host}
-          whitelist={[whitelist]}
-        />
-      </div>
+      <h1 className="text-red-500">Home</h1>
     </div>
   );
 }
