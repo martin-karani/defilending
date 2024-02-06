f07798bc595265c892a427c9c8809611567bb322
martin-karani <karanimartinmk@gmail.com>
2024-02-07 02:41:38 +0300

    feat: add dynamic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,43 @@
-import PlugConnect from '@psychedelic/plug-connect'
-import React, { useState } from 'react'
+import PlugConnect from '@psychedelic/plug-connect';
+import { useState } from 'react';
+
+const whitelist = process.env.CANISTER_ID_FRONTEND || '';
+
+const host = 'http://127.0.0.1:4943';
 
 const Navbar = () => {
-    const handleConnectWallet = async () => {
-        console.log('Connecting to the Connect Wallet');
-        const result = await window.ic.plug.requestBalance();
-        console.log({ result });
-      }
+  const handleConnectWallet = async () => {
+    console.log('Connecting to the Connect Wallet');
+    const result = await window.ic.plug.requestBalance();
+    console.log({ result });
+  };
 
-      const [currentPage, setCurrentPage] = useState('home')
-      
+  const [currentPage, setCurrentPage] = useState('home');
+
   return (
-    <div className='bg-black flex justify-between px-8 text-white py-3 items-center'>
-        <h2 className='text-xl font-semibold'>Difi-<span className='text-blue-600'>lending</span></h2>
-        <div>
-            <ul className='flex gap-6 items-center'>
-                <li 
-                >Home</li>
-                <li>Dashboard</li>
-                <li>Market</li>
-            </ul>
+    <div className="bg-black flex justify-between px-8 text-white py-3 items-center">
+      <h2 className="text-xl font-semibold">
+        Difi-<span className="text-blue-600">lending</span>
+      </h2>
+      <div>
+        <ul className="flex gap-6 items-center">
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>Market</li>
+        </ul>
+      </div>
+      <div>
+        <div className="bg-slate-00">
+          <PlugConnect
+            title="Connect Wallet"
+            onConnectCallback={handleConnectWallet}
+            host={host}
+            whitelist={[whitelist]}
+          />
         </div>
-        <div>
-        <PlugConnect
-          title="Connect Wallet"
-          onConnectCallback={handleConnectWallet}
-          host="http://127.0.0.1:4943"
-          whitelist={['bd3sg-teaaa-aaaaa-qaaba-cai']}
-        />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
